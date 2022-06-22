dd98d2ce700943f4b4bf86008a10c2434e775d06
Shakeel <shakeelafridi7659@gmail.com>
2022-06-23 01:34:05 +0500

    allow host

--- video_analysis/settings.py
+++ video_analysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-671p$x)260u)qjt6d-&d*9(%x-g8ym8^fpu(3n&vwu5-sqs=kn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
