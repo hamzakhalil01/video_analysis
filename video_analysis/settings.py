f672348a25926dce27c112fc40139c588a294886
acalk <hamzashah90201@gmail.com>
2022-07-16 10:59:18 +0500

    added video upload option

--- video_analysis/settings.py
+++ video_analysis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -136,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+VIDEO_STORAGE = os.path.join(BASE_DIR, '/video')
