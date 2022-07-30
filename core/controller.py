38c517698e66ea42fb08f317b65f2528f9050451
acalk <hamzashah90201@gmail.com>
2022-07-30 15:41:24 +0500

    update in video storage

--- core/controller.py
+++ core/controller.py
@@ -3,6 +3,7 @@ from core.helper import create_response, get_default_query_param
 from rest_framework.pagination import LimitOffsetPagination
 from django.db.models import Q
 
+
 class ProjectController:
     serializer_class = ProjectSerializer
 
@@ -14,9 +15,8 @@ class ProjectController:
         else:
             return create_response({}, "Something went wrong", 400)
 
-
     def get_listing(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             limit = get_default_query_param(request, "limit", None)
             offset = get_default_query_param(request, "offset", None)
             search = get_default_query_param(request, "search", None)
@@ -31,7 +31,7 @@ class ProjectController:
         return create_response(self.serializer_class(data, many=True).data, "Success", 200)
 
     def update_listing(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response({}, "user id not provided", 400)
         else:
             instance = self.serializer_class.Meta.model.objects.filter(id=request.query_params.get("id")).first()
@@ -43,9 +43,9 @@ class ProjectController:
             return create_response(serializer_data.data, "Success", 200)
 
     def delete_listing(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response({}, "user id not provided", 400)
-        self.serializer_class.Meta.model.objects.filter(id= request.query_params.get("id")).delete()
+        self.serializer_class.Meta.model.objects.filter(id=request.query_params.get("id")).delete()
         return create_response({}, "Success", 200)
 
 
@@ -60,14 +60,14 @@ class VideoController:
         return create_response({}, serializer_data.errors, 400)
 
     def get(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             limit = get_default_query_param(request, "limit", None)
             offset = get_default_query_param(request, "offset", None)
 
             data = self.serializer_class.Meta.model.objects.all()
             if limit and offset:
-                paginatin = LimitOffsetPagination()
-                data = paginatin.paginate_queryset(data, request)
+                pagination = LimitOffsetPagination()
+                data = pagination.paginate_queryset(data, request)
             return create_response(self.serializer_class(data, many=True).data, "Success", 200)
 
         else:
@@ -77,7 +77,7 @@ class VideoController:
             return create_response(self.serializer_class(instance).data, "Success", 200)
 
     def update(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response({}, "Video id not provided", 400)
         else:
             instance = self.serializer_class.Meta.model.objects.filter(id=request.query_params.get("id")).first()
@@ -90,7 +90,7 @@ class VideoController:
             create_response({}, "Something went wrong", 400)
 
     def delete(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response({}, "Video id not provided", 400)
 
         self.serializer_class.Meta.model.objects.filter(id=request.query_params.get("id")).delete()
@@ -108,7 +108,7 @@ class QuestionsController:
         create_response({}, serializer_data.errors, 400)
 
     def get(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response(self.serializer_class(
                 self.serializer_class.Meta.model.objects.all(), many=True).data,
                                    "Success", 200)
@@ -118,7 +118,7 @@ class QuestionsController:
         return create_response(self.serializer_class(instance).data, "Success", 200)
 
     def update(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response({}, "Question id not provided", 400)
         instance = self.serializer_class.Meta.model.objects.filter(id=request.query_params.get("id")).first()
         if not instance:
@@ -129,12 +129,10 @@ class QuestionsController:
             return create_response(serialized_data.data, "Success", 200)
         return create_response({}, serialized_data.errors, 400)
 
-
     def delete(self, request):
-        if not "id" in request.query_params:
+        if "id" not in request.query_params:
             return create_response({}, "Question id not provided", 400)
 
         self.serializer_class.Meta.model.objects.filter(
             id=request.query_params.get("id")).delete()
         return create_response({}, "Success", 200)
-
--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Project(models.Model):
     name = models.CharField(max_length=100, unique=True, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
